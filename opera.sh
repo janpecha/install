5a61e1ef69d4b13f1c744ed393aff8420c6d6088
Jan Pecha <janpecha@email.cz>
2013-04-29 08:34:36 +0200

    opera: added a note
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- opera.sh
+++ opera.sh
@@ -3,5 +3,7 @@
 # instalace Opera
 
 echo "Download from opera.com, double-click, click on Install package"
+echo "Opera as default browser:"
+echo "Nastavení systému > Podrobnosti > Výchozí aplikace"
 read -p "Press [Enter] key to exit..."
 
