abe85e5aadb5c7d31959fb154d9eb471ea06d9d1
Jan Pecha <janpecha@email.cz>
2013-04-29 08:14:49 +0200

    added ambiance-blue-theme script
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ ambiance-blue-theme.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# instalace ambiance-blue-theme
+# sudo add-apt-repository ppa:satyajit-happy/themes
+# sudo apt-get update && sudo apt-get install ambiance-blue-theme
+
+echo "Uprava stylu:"
+echo "/usr/share/themes/Ambiance Blue/gtk3.0/apps/gnome-panel.css"
+echo "překopírovaní souboru s fixem"
+read -p "Press [Enter] key to exit..."
+
