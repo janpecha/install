fca2b2400c12617ee4076b44e01b26d13c4d0b41
Jan Pecha <janpecha@email.cz>
2013-11-25 20:59:52 +0100

    PHP-5.4: added more PHP modules, PHP error-handling settings
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- php-5.4.sh
+++ php-5.4.sh
@@ -13,11 +13,48 @@ apt-get update
 echo "Install PHP 5"
 apt-get install php5
 
+# PHP modules
 echo "Install PHP 5 CURL"
 apt-get install php5-curl
 
 echo "Install PHP 5 Readline"
 apt-get install php5-readline
 
-# read -p "Press [Enter] key to exit..."
+echo "Install PHP 5 MySQL"
+apt-get install php5-mysql
+
+echo "Install PHP 5 GD"
+apt-get install php5-gd
+
+echo "Install PHP 5 intl"
+apt-get install php5-intl
+
+echo "Install PHP 5 Imagick"
+apt-get install php5-imagick
+
+echo "Install PHP 5 mcrypt"
+apt-get install php5-mcrypt
+
+echo "Install PHP 5 SQLite"
+apt-get install php5-sqlite
+
+echo "Install PHP 5 XML-RPC"
+apt-get install php5-xmlrpc
+
+echo "Install PHP 5 XSL"
+apt-get install php5-xsl
+
+echo "Install PHP 5 xdebug"
+apt-get install php5-xdebug
+
+echo ""
+echo "Upravit nastaveni PHP (pro Apache2 a CLI):"
+echo " - short_open_tag = Off"
+echo " - error_reporting = E_ALL | E_STRICT"
+echo " - display_errors = On"
+echo " - display_startup_errors = On"
+echo " - track_errors = On"
+
+echo ""
+read -p "Press [Enter] key to exit..."
 
