c49f81c7f26a7c9b9905dea42233b9b2b993e340
Jan Pecha <janpecha@email.cz>
2013-11-25 21:00:10 +0100

    Added gnome3-class.sh
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ gnome3-classic.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# instalace GNOME3 classic
+# http://linuxg.net/how-to-install-gnome-classic-on-ubuntu-13-04-12-10-12-04/
+
+# Tips (from OMGbuntu)
+# http://www.omgubuntu.co.uk/2012/03/gnome-classic-in-ubuntu-12-04-its-like-nothing-ever-changed
+# To move/add/edit panels or applets press Alt+Right Click
+# Compiz effects are supported – but you’ll need to install the CCSM application to enable specific effects
+# Non-Compiz users can create a less flat-look by enabling ‘shadow’ in Metacity
+# Support for themes, etc.
+
+# or apt-get install gnome-panel
+echo "GNOME classic"
+apt-get install gnome-session-fallback
+
+# read -p "Press [Enter] key to exit..."
+
