9bd32246ae1cd277de3f79ea1da8321e71b62b11
Jan Pecha <janpecha@email.cz>
2013-11-25 23:07:47 +0100

    Apache2: added enabling of mod_rewrite
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- apache2.sh
+++ apache2.sh
@@ -9,8 +9,20 @@ echo ""
 echo "Change DocumentRoot:"
 echo " - edit /etc/apache2/sites-available/default"
 echo " - change all /var/www to your directory"
-echo " - restart Apache:"
-echo " ---- $ /etc/init.d/apache2 restart"
+echo ""
+
+# http://askubuntu.com/questions/48362/how-to-enable-mod-rewrite-in-apache
+# https://www.digitalocean.com/community/articles/how-to-set-up-mod_rewrite
+echo "Enable mod_rewrite:"
+echo " - $ a2enmod rewrite"
+echo " - edit /etc/apache2/sites-available/default"
+echo " - set AllowOverride for your DocumentRoot to value All (or FileInfo)"
+echo ""
+
+echo "Restart Apache2:"
+echo " - $ service apache2 restart"
+echo "or"
+echo " - $ /etc/init.d/apache2 restart"
 echo ""
 
 read -p "Press [Enter] key to exit..."
