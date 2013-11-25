cdee3892f350133b2324ee48c02297dee0362b71
Jan Pecha <janpecha@email.cz>
2013-11-25 13:43:17 +0100

    Added set.git.sh
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ set.git.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# nastavení Gitu
+# http://git-scm.com/book/cs/Úvod-První-nastavení-systému-Git
+echo "Git settings"
+echo "Set your name:"
+echo "$ git config --global user.name \"John Doe\""
+read -p "Press [Enter] key to continue..."
+echo "Set your email:"
+echo "$ git config --global user.email johndoe@example.com"
+read -p "Press [Enter] key to exit..."
+
