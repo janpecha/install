12606517947a89f9b2f32966306a3399e42bfd37
Jan Pecha <janpecha@email.cz>
2013-11-25 21:28:23 +0100

    Added defaults Nautilus scripts
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- extend.nautilus.sh
+++ extend.nautilus.sh
@@ -5,3 +5,5 @@ apt-get install nautilus-actions
 apt-get install nautilus-scripts-manager
 apt-get install nautilus-open-terminal
 
+echo "Nakopiruj slozku data/nautilus-scripts do ~/.gnome2"
+read -p "Press [Enter] key to exit..."
