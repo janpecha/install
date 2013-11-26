c4775067950b49023d24b3107727ed416d41fadd
Jan Pecha <janpecha@email.cz>
2013-11-26 19:52:34 +0100

    Ambiance-Blue-Theme: updated - add repo and install
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- ambiance-blue-theme.sh
+++ ambiance-blue-theme.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # instalace ambiance-blue-theme
-# sudo add-apt-repository ppa:satyajit-happy/themes
-# sudo apt-get update && sudo apt-get install ambiance-blue-theme
+add-apt-repository ppa:satyajit-happy/themes
+apt-get update && apt-get install ambiance-blue-theme
 
+echo ""
 echo "Uprava stylu:"
 echo "/usr/share/themes/Ambiance Blue/gtk3.0/apps/gnome-panel.css"
 echo "překopírovaní souboru s fixem"
+echo ""
 echo "Ubuntu Tweak - Themes - Gtk Theme a Window Theme nastavit na Ambiance Blue"
 echo "Nebo Gnome Tweak Tool (apt-get install gnome-tweak-tool) - Theme - Gtk+ Theme a Window Theme nastavit na Ambiance Blue"
+echo ""
 read -p "Press [Enter] key to exit..."
 
