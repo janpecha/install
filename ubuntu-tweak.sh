f3a6c8872d9be07d61d764a6316659b2dde874f2
Jan Pecha <janpecha@email.cz>
2013-11-26 19:51:02 +0100

    Ubuntu Tweak: added "Presun tlacitek okna napravo"
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- ubuntu-tweak.sh
+++ ubuntu-tweak.sh
@@ -5,3 +5,11 @@ add-apt-repository ppa:tualatrix/ppa
 apt-get update
 apt-get install ubuntu-tweak
 
+echo ""
+echo "Presun tlacitek oken napravo:"
+echo " - Nastaveni systemu > Vylepseni Ubuntu"
+echo " - Vyladeni > Okno"
+echo " - Poradi ovladacich tlacitek na okne: nastavit => Vpravo"
+echo ""
+read -p "Press [Enter] key to exit..."
+echo ""
