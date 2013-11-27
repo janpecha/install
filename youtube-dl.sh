b62b5a6d6b754dd1b8f57b5583b18117d1fa1a94
Jan Pecha <janpecha@email.cz>
2013-11-27 21:42:07 +0100

    Added youtube-dl
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ youtube-dl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# instalace youtube-dl
+echo ""
+echo "Instalace z repozitare:"
+echo " - $ apt-get install youtube-dl"
+echo ""
+echo "Instalace z webu nastroje:"
+echo " - web: http://rg3.github.io/youtube-dl/download.html"
+echo " - $ sudo curl https://yt-dl.org/downloads/2013.11.26/youtube-dl -o /usr/local/bin/youtube-dl"
+echo " - $ sudo chmod a+x /usr/local/bin/youtube-dl"
+echo ""
+
+read -p "Press [Enter] key to exit..."
+
