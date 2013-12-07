90cb6a719048846d3d289ceabf64bf71494e6b25
Jan Pecha <janpecha@email.cz>
2013-12-07 09:34:42 +0100

    Added nodejs and bower
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ bower.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# instalace Bower
+echo "Nainstalujte Node.JS"
+read -p "Press [Enter] key to exit..."
+
+npm install -g bower
+
---
+++ nodejs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# instalace Node.JS
+# https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
+apt-get update
+apt-get install -y python-software-properties python g++ make
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get update
+apt-get install nodejs
+
