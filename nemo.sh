8cf9b15b9568576d563335ed61131562c132945c
Jan Pecha <janpecha@email.cz>
2014-01-11 15:00:19 +0100

    Added Nemo (Nautilus alternative)

---
+++ nemo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# instalace Nemo
+
+add-apt-repository ppa:webupd8team/nemo
+apt-get update
+apt-get install nemo
+apt-get install nemo-terminal
+
+cp -r data/nautilus-scripts/* ~/.gnome2/nemo-scripts/
+
+# drawing desktop icons by Nemo (disables Nautilus)
+# http://www.webupd8.org/2013/10/install-nemo-with-unity-patches-and.html
+gsettings set org.gnome.desktop.background show-desktop-icons false
+
+# Nemo as the default file manager (instead of Nautilus)
+# http://www.webupd8.org/2013/10/install-nemo-with-unity-patches-and.html
+xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
+
+
+echo ""
+echo "************************************************************"
+echo "*"
+echo "* Predvolby > Chovani"
+echo "* > Spustitelne soubory: Vzdy se ptat"
+echo "* > [x] Nabidnout prikaz 'Smazat'"
+echo "*"
+echo "************************************************************"
+echo ""
+read -p "Press [Enter] key to exit..."
+
