581ea649c422e1f83cd204fae3de0df769c7841b
Jan Pecha <janpecha@email.cz>
2014-01-11 14:55:54 +0100

    Apache 2: updated

--- apache2.sh
+++ apache2.sh
@@ -1,29 +1,66 @@
 #!/bin/bash
 
 # instalace Apache2
-echo "Apache2"
+echo ""
+echo "********************************"
+echo "*"
+echo "* Apache2"
+echo "*"
+echo "********************************"
+echo ""
 
 apt-get install apache2
 
 echo ""
-echo "Change DocumentRoot:"
-echo " - edit /etc/apache2/sites-available/default"
-echo " - change all /var/www to your directory"
+echo "********************************"
+echo "*"
+echo "* Change DocumentRoot:"
+echo "*  - edit /etc/apache2/sites-available/000-default.conf"
+echo "*  - change all /var/www to your directory"
+echo "*"
+echo "********************************"
 echo ""
+
+gedit /etc/apache2/sites-available/000-default.conf
 
 # http://askubuntu.com/questions/48362/how-to-enable-mod-rewrite-in-apache
 # https://www.digitalocean.com/community/articles/how-to-set-up-mod_rewrite
-echo "Enable mod_rewrite:"
-echo " - $ a2enmod rewrite"
-echo " - edit /etc/apache2/sites-available/default"
-echo " - set AllowOverride for your DocumentRoot to value All (or FileInfo)"
 echo ""
-
-echo "Restart Apache2:"
-echo " - $ service apache2 restart"
-echo "or"
-echo " - $ /etc/init.d/apache2 restart"
+echo "********************************"
+echo "*"
+echo "* Enable mod_rewrite:"
+echo "*"
+echo "********************************"
 echo ""
+a2enmod rewrite
+# echo " - $ a2enmod rewrite"
+echo ""
+echo "********************************"
+echo "*"
+echo "*  - edit /etc/apache2/sites-available/000-default.conf"
+echo "*  - set AllowOverride for your DocumentRoot to value All (or FileInfo)"
+echo "*"
+echo "* <Directory /path/to/your/docroot>"
+echo "*     AllowOverride All"
+echo "*     Options Indexes FollowSymLinks"
+echo "* </Directory>"
+echo "*"
+echo "********************************"
+echo ""
+gedit /etc/apache2/sites-available/000-default.conf
 
-read -p "Press [Enter] key to exit..."
+read -p "Press [Enter] key to continue..."
+
+echo ""
+echo "********************************"
+echo "*"
+echo "Restarting Apache2..."
+echo "*"
+echo "********************************"
+echo ""
+service apache2 restart
+#echo " - $ service apache2 restart"
+#echo "or"
+#echo " - $ /etc/init.d/apache2 restart"
+echo ""
 
