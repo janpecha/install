8562b08375935966fe6198fad6c90ce83ea203e6
Jan Pecha <janpecha@email.cz>
2014-01-11 14:48:45 +0100

    Added SublimeText 3

---
+++ sublime-3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# instalace SublimeText3
+echo ""
+echo "************************************************************"
+echo "*"
+echo "* Stahnout z http://www.sublimetext.com/3"
+echo "* nainstalovat rucne (dvojklik na stazeny balik)"
+echo "*"
+echo "************************************************************"
+echo ""
+read -p "Press [Enter] key to exit..."
+
