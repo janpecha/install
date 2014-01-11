289e5f634fb7b1a26a1ad695fb667f33e614b68a
Jan Pecha <janpecha@email.cz>
2014-01-11 14:56:12 +0100

    Birdie: updated

--- birdie.sh
+++ birdie.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # instalace Birdie
-add-apt-repository  ppa:birdie-team/stable
+add-apt-repository ppa:gnome3-team/gnome3-next
+add-apt-repository ppa:birdie-team/daily
 apt-get update
 apt-get install birdie
