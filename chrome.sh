73f2bdd26500949f331209164873b907b47b4e35
Jan Pecha <janpecha@email.cz>
2014-01-11 14:47:50 +0100

    Added Google Chrome

---
+++ chrome.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# instalace Chrome
+
+echo ""
+echo "********************************************************************"
+echo "*"
+echo "* Stahnete z http://www.google.com/intl/cs/chrome/ a nainstalujte"
+echo "* Repo bude pridano automaticky."
+echo "*"
+echo "********************************************************************"
+echo ""
+read -p "Press [Enter] key to exit..."
+
