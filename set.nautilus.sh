e800a14f58b28647973a41c0553ff3cd73f3baa8
Jan Pecha <janpecha@email.cz>
2014-01-11 14:58:08 +0100

    Added set.nautilus

---
+++ set.nautilus.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# nastaveni Nautila
+echo ""
+echo "************************************************************"
+echo "*"
+echo "* Predvolby > Chovani"
+echo "* > Spustitelne soubory: Vzdy se ptat"
+echo "* > [x] Nabidnout prikaz 'Smazat'"
+echo "*"
+echo "************************************************************"
+echo ""
+read -p "Press [Enter] key to exit..."
+
