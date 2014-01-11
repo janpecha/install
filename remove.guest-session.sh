a9f5e26047e55d41c3b7a99626560e2f8133c116
Jan Pecha <janpecha@email.cz>
2014-01-11 14:54:18 +0100

    Remove.guest-session: updated

--- remove.guest-session.sh
+++ remove.guest-session.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # zakaz Sezeni hosta
-# http://askubuntu.com/questions/62564/how-do-i-disable-the-guest-session
-echo "/etc/lightdm/lightdm.conf"
-echo "nastavit / přidat řádek:"
-echo "allow-guest=false"
-read -p "Press [Enter] key to exit..."
+# http://www.mattfischer.com/blog/?p=343
+/usr/lib/lightdm/lightdm-set-defaults --allow-guest false
 
