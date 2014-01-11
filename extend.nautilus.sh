0149012c4ac71ab86e22369405e21ce510965b01
Jan Pecha <janpecha@email.cz>
2014-01-11 15:00:03 +0100

    Extend.nautilus: disabled nautilus-actions & scripts

--- extend.nautilus.sh
+++ extend.nautilus.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # rozsireni Nautilus
-apt-get install nautilus-actions
-apt-get install nautilus-scripts-manager
+#apt-get install nautilus-actions
+#apt-get install nautilus-scripts-manager
 apt-get install nautilus-open-terminal
 
-echo "Nakopiruj slozku data/nautilus-scripts do ~/.gnome2"
-read -p "Press [Enter] key to exit..."
+# cp -r data/nautilus-scripts/* ~/.local/share/nautilus/scripts/
+# cp -r data/nautilus-scripts/* ~/.gnome2/nautilus-scripts/
+# echo "Nakopiruj slozku data/nautilus-scripts do ~/.gnome2"
+# read -p "Press [Enter] key to exit..."
