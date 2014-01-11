b185e72f3fb56c273c30ca07ff201bf37038e9e3
Jan Pecha <janpecha@email.cz>
2014-01-11 14:57:59 +0100

    Added set.window.buttons.right

---
+++ set.window.buttons.right.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# nastaveni tlacitek oken doprava
+# http://askubuntu.com/questions/370038/ubuntu-13-10-move-the-window-buttons-to-the-right
+gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
+
