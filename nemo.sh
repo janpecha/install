90c757cd0ea6f6862e29ce5fb42036356e10a2ff
Jan Pecha <janpecha@email.cz>
2014-01-17 20:14:12 +0100

    Nemo: disabled desktop-drawing

--- nemo.sh
+++ nemo.sh
@@ -11,7 +11,10 @@ cp -r data/nautilus-scripts/* ~/.gnome2/nemo-scripts/
 
 # drawing desktop icons by Nemo (disables Nautilus)
 # http://www.webupd8.org/2013/10/install-nemo-with-unity-patches-and.html
-gsettings set org.gnome.desktop.background show-desktop-icons false
+# gsettings set org.gnome.desktop.background show-desktop-icons false
+# Remove settings:
+# gsettings set org.nemo.desktop show-desktop-icons false
+# gsettings set org.gnome.desktop.background show-desktop-icons true
 
 # Nemo as the default file manager (instead of Nautilus)
 # http://www.webupd8.org/2013/10/install-nemo-with-unity-patches-and.html
